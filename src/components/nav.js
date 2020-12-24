ea48f59c5adc1e020801ddc1d4004d5a6f567885
Daniel Huynh <danielhuynh97@gmail.com>
2020-12-23 20:30:03 -0800

    removed bloat files

--- src/components/nav.js
+++ src/components/nav.js
@@ -41,7 +41,7 @@ const StyledHeader = styled.header`
     css`
       height: var(--nav-scroll-height);
       transform: translateY(calc(var(--nav-scroll-height) * -1));
-      box-shadow: 0 10px 300px -10px var(--main-black);
+      box-shadow: 0 10px 30px -10px var(--main-black);
     `};
 
   @media (max-width: 1080px) {

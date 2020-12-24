ca59237ec30b8b3ae79349e8722d25745e25365d
Daniel Huynh <danielhuynh97@gmail.com>
2020-12-24 13:49:57 -0800

    fixed scroll button

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -39,71 +39,8 @@ const StyledHeroSection = styled.section`
   .email-link {
     ${({ theme }) => theme.mixins.bigButton};
       background: --main-blue;
-    margin-top: 0px;
+    margin-top: 40px;
     font-size: var(--fz-lg);
-  }
-
-  .demo a {
-    position: absolute;
-    bottom: 0px;
-    left: 50%;
-    z-index: 2;
-    display: inline-block;
-    -webkit-transform: translate(0, -50%);
-    transform: translate(0, -50%);
-    color: var(--main-black);
-    font : normal 400 20px/1 'Josefin Sans', sans-serif;
-    letter-spacing: .1em;
-    text-decoration: none;
-    transition: opacity .3s;
-  }
-  .demo a:hover {
-    opacity: .5;
-  }
-
-  #section06 a {
-    padding-top: 70px;
-  }
-  #section06 a span {
-    position: absolute;
-    top: 0;
-    left: 50%;
-    width: 24px;
-    height: 24px;
-    margin-left: -12px;
-    border-left: 1px solid var(--main-black);
-    border-bottom: 1px solid var(--main-black);
-    -webkit-transform: rotateZ(-45deg);
-    transform: rotateZ(-45deg);
-    -webkit-animation: sdb06 1.5s infinite;
-    animation: sdb06 1.5s infinite;
-    box-sizing: border-box;
-  }
-  @-webkit-keyframes sdb06 {
-    0% {
-      -webkit-transform: rotateY(0) rotateZ(-45deg) translate(0, 0);
-      opacity: 0;
-    }
-    50% {
-      opacity: 1;
-    }
-    100% {
-      -webkit-transform: rotateY(720deg) rotateZ(-45deg) translate(-20px, 20px);
-      opacity: 0;
-    }
-  }
-  @keyframes sdb06 {
-    0% {
-      transform: rotateY(0) rotateZ(-45deg) translate(0, 0);
-      opacity: 0;
-    }
-    50% {
-      opacity: 1;
-    }
-    100% {
-      transform: rotateY(720deg) rotateZ(-45deg) translate(-20px, 20px);
-      opacity: 0;
-    }
   }
 `;
 
@@ -121,10 +58,7 @@ const Hero = () => {
   const four = <p>I'm currently an undergrad studying Business and Computer Science at the University of California, San Diego.
                   I hope to be able to intersect both of my passions of finance and computer science into meaningful work.</p>;
   const five = (
-    <section id="section06" class="demo">
-      <a href="#about"><span></span></a>
-    </section>
-
+    <a href= '#about' className="email-link">‏‏‎see more</a>
   );
 
   const items = [one, two, three, four, five];

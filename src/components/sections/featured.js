a99afcded66243062d020522a9efc66523220597
Daniel Huynh <danielhuynh97@gmail.com>
2020-12-29 21:28:54 -0800

    [featured]: decreased padding between projects

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -13,7 +13,7 @@ const StyledProject = styled.div`
   align-items: center;
 
   &:not(:last-of-type) {
-    margin-bottom: 125px;
+    margin-bottom: 100px;
 
     @media (max-width: 768px) {
       margin-bottom: 70px;
